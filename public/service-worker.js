d3b926a4f3392fdefd045a191a6ee392a4f1e3d3
vayne <miguel.osorio19@yahoo.com>
2022-03-20 02:30:45 -0500

    added relative paths of files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,25 @@
+// collection of files to cache
+const FILES_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './css/styles.css',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png',
+    './js/index.js',
+    './idb.js'
+];
+
+const APP_PREFIX = 'BudgetTracker-'; // app name
+const VERSION = 'version_01'; // version iteration
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 self.addEventListener('install', function(e) {
     
 })
